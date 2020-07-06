b96476f1c8c2b5b2542637cbf2bf951396e123c9
Nikhil Kommuri <44361996+NikhilKommuri@users.noreply.github.com>
2020-07-06 11:34:59 -0400

    Added rel="noopener noreferrer"

--- src/components/Social.js
+++ src/components/Social.js
@@ -4,10 +4,10 @@ class Social extends Component {
     render() {
         return (
             <div class="social">
-            <a href="https://www.linkedin.com/in/nikhilkommuri/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
-            <a href="https://github.com/NikhilKommuri" target="_blank"><i class="fab fa-github"></i></a>
-            <a href="https://www.instagram.com/nikhil_kommuri/" target="_blank"><i class="fab fa-instagram"></i></a>
-            <a href="https://www.facebook.com/nikhil.kommuri" target="_blank"><i class="fab fa-facebook-f"></i></a>
+            <a href="https://www.linkedin.com/in/nikhilkommuri/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
+            <a href="https://github.com/NikhilKommuri" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
+            <a href="https://www.instagram.com/nikhil_kommuri/" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i></a>
+            <a href="https://www.facebook.com/nikhil.kommuri" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook-f"></i></a>
             </div>
             )
         }
